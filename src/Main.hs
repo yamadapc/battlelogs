8b613ad7eab0c649639b2439c329424fd93e7090
yamadapc <tacla.yamada@gmail.com>
2014-09-12 20:17:31 -0300

    Fix the code to append to `~/.battlelogs.md`
    This is pretty usable now. I'd like to be able to init battlelogs to
    different paths (say a git project), but this is already a decent
    script.

--- src/Main.hs
+++ src/Main.hs
@@ -17,31 +17,39 @@
 import Control.Exception (finally)
 import Control.Monad (liftM)
 import Data.Time (getCurrentTime, formatTime)
-import System.Directory (getTemporaryDirectory, removeFile)
+import System.Directory (getCurrentDirectory, getTemporaryDirectory, removeFile)
 import System.Environment (getEnv)
+import System.FilePath ((</>))
 import System.IO -- (SeekMode(..), Handle, hClose, hFlush, hGetContents,
                  -- hPutStrLn, hSeek, openTempFile, stdout)
 import System.IO.Error (catchIOError)
 import System.Locale (defaultTimeLocale)
 import System.Process (system)
 
+
 main :: IO ()
 main = withSystemTempFile "battleLogs.md" $ \fp _ -> do
     -- Capture output from the standard editor
     openInEditor fp
+
+    -- Append entry to battlelogs
     fh <- openFile fp ReadMode
-
-    -- Insert header to content
-    appendHeader fh
+    header <- getHeader
     c <- hGetContents fh
+    targetPth <- battleLogsPth
 
-    -- Append entry to output
-    targetFh <- openFile "./output.md" AppendMode
-    hPutStrLn targetFh c
+    targetFh <- openFile targetPth AppendMode
+    hPutStrLn targetFh (header ++ c)
 
     -- Clean-up
     hClose fh
     hClose targetFh
+
+battleLogsPth :: IO FilePath
+battleLogsPth =  liftM logsPthOf logsDir
+    where logsDir = catchIOError (getEnv "HOME") (const getCurrentDirectory)
+          logsPthOf = (</> ".battlelogs.md")
+
 
 openInEditor :: FilePath -> IO ()
 openInEditor fp = do
@@ -55,10 +63,7 @@ prompt str = putStr str >> hFlush stdout >> getLine
 
 getHeader :: IO String
 getHeader = liftM formatTime' getCurrentTime
-  where formatTime' = formatTime defaultTimeLocale "%x %X"
-
-appendHeader :: Handle -> IO ()
-appendHeader fh = getHeader >>= hPutStrLn fh
+  where formatTime' = formatTime defaultTimeLocale "%x %X\n"
 
 -- From "Real World Haskell"
 withSystemTempFile :: String -> (FilePath -> Handle -> IO a) -> IO a
